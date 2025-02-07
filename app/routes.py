9577010b69b658b1bc1f48221048bc954443b9b5
PascalAndermatt1994 <pascal.fritschi@student.ipso.ch>
2025-02-07 15:15:54 +0100

    login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
 from app import app
+from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "<h1>Hello, Timo</h1>"
+    user = {'username': 'Jochen'}
+    posts = [
+ {
+ 'author': {'username': 'Paul'},
+ 'body': 'Schöner Abend hier in Zürich!'
+ },
+ {
+ 'author': {'username': 'Susanne'},
+ 'body': 'Der Unterricht war heute mal gut!'
+ }
+ ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
